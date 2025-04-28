ed41d4db224088c22f53b006c52d0e0f32f2bfe6
abhi891009 <33829011+abhi891009@users.noreply.github.com>
2025-04-28 09:05:52 +0000

    Changes the commit

--- main.tf
+++ main.tf
@@ -1,7 +1,25 @@
-resource "null_resource" "Null_Resource" {
-  count = length(var.commands)
+resource "aws_instance" "MyEc2Instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
 
-  provisioner "local-exec" {
-    command = var.commands[count.index]
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y apache2
+              EOF
+
+  tags = {
+    Name = "MyEc2Instance"
+  }
+
+  provisioner "remote-exec" {
+    inline = var.commands
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/id_rsa")
+      host        = self.public_ip
+    }
   }
 }
--- variable.tf
+++ variable.tf
@@ -1,6 +1,17 @@
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  type    = string
+  default = "ami-084568db4383264d4" 
+}
+
 variable "commands" {
   type = list(string)
   default = [
-    "echo 'Running dynamic commands'"
+    "sudo apt-get update",
+    "sudo apt-get install -y nginx"
   ]
 }
